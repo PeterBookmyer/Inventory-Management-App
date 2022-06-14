91eecd90d50a14146601d9454256252d8e0e6faa
Melissa Lycan <melissamlycan@gmail.com>
2022-06-14 15:03:14 -0600

    updated controllers added user login links to withAuth

---
+++ controllers/user-dashboard.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const { User } = require("../models/");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      where: {
+        userId: req.session.userId,
+      },
+    });
+
+    const users = userData.map((User) => User.get({ plain: true }));
+
+    res.render("all-users-admin", {
+      layout: "dashboard",
+      posts,
+    });
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
+router.get("/new", withAuth, (req, res) => {
+  res.render("new-user", {
+    layout: "dashboard",
+  });
+});
+
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render("edit-post", {
+        layout: "dashboard",
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
+module.exports = router;
