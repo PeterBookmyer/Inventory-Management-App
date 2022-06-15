3ec6dbb348fbd777f1fe08106dd788f97d73b191
Melissa Lycan <melissamlycan@gmail.com>
2022-06-15 12:00:16 -0600

    last seed data for pricing

--- models/Pricing.js
+++ models/Pricing.js
@@ -1,44 +1,44 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Pricing extends Model {};
+class Pricing extends Model {}
 
-Pricing.init (
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        cost: {
-            type: DataTypes.DECIMAL(18, 2),
-            allowNull: false,
-            defaultValue: 0,
-        },
-        sales_price: {
-            type: DataTypes.DECIMAL(18, 2),
-            allowNull: false,
-            defaultValue: 0,
-        },
-        order_link: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        inventory_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'inventory',
-                key: 'id',
-            },
-        },
+Pricing.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'pricing',
+    cost: {
+      type: DataTypes.DECIMAL(18, 2),
+      allowNull: false,
+      defaultValue: 0,
     },
+    sales_price: {
+      type: DataTypes.DECIMAL(18, 2),
+      allowNull: false,
+      defaultValue: 0,
+    },
+    order_link: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    inventory_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "inventory",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "pricing",
+  }
 );
 
 module.exports = Pricing;
