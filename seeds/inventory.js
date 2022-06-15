1465f75170f6effa65af6cff0e99527146d0d64d
Melissa Lycan <melissamlycan@gmail.com>
2022-06-15 11:42:45 -0600

    added inventory seeds connected to the models

--- seeds/inventory.js
+++ seeds/inventory.js
@@ -1,85 +1,54 @@
 const { Inventory } = require("../models");
 
-const itemData = [
+const inventoryData = [
   {
-    name: "Large Sofa 110in",
-    image: "href",
-    exhibition_date: "March 30, 2018",
-    gallery_id: 1,
-    filename: "01-blossoming-apricot.jpg",
-    description:
-      "Branches with pink apricot blossoms against a blue background.",
+    name: "CD: Forgotten",
+    image_file: "https://www.youthonrecord.org/sites/default/files/2018-09/Nordic%20Daughter%20Sticker%20-%20Nordic%20Daughter.jpg",
+    current_stock: "100",
   },
   {
-    title: "Cosmos Flowers",
-    artist: "WStudio",
-    exhibition_date: "May 05, 2017",
-    gallery_id: 1,
-    filename: "02-cosmos-flowers.jpg",
-    description: "Pink cosmos flowers against a blue sky.",
+    name: "CD: The Path",
+    image_file: "https://resources.tidal.com/images/67a4a7d1/5d80/40c5/8119/3c58ee482092/640x640.jpg",
+    current_stock: "100",
   },
   {
-    title: "Sand + Sea = Summer",
-    artist: "S_Photo",
-    exhibition_date: "June 10, 2019",
-    gallery_id: 2,
-    filename: "03-sand-sea-summer.jpg",
-    description: "Sandy beach with the blue sea and sky in the background.",
+    name: "Hoodie",
+    image_file: "https://mockup-api.teespring.com/v3/image/dlnkK9vxd2EVaHxTsZQrmx1G3nk/800/800.jpg",
+    current_stock: "50",
   },
   {
-    title: "Beach Chairs",
-    artist: "icemanphotos",
-    exhibition_date: "July 4, 2020",
-    gallery_id: 2,
-    filename: "04-beach-chairs.jpg",
-    description: "Two beach chairs under a beach umbrella on the beach.",
+    name: "Shot Glass",
+    image_file: "href",
+    current_stock: "100",
+  },{
+    name: "Drinking Horn",
+    image_file: "https://cdn.shopify.com/s/files/1/1408/0936/products/1_Custom_a8af9004-1730-4f6a-b530-4800c71b96ee_750x750.jpg?v=1625414418",
+    current_stock: "200",
   },
   {
-    title: "Beach Sunrise",
-    artist: "VRstudio",
-    exhibition_date: "August 14, 2016",
-    gallery_id: 2,
-    filename: "05-beach-sunrise.jpg",
-    description: "Sun setting in the horizon with waves lapping the shore.",
+    name: "Rune Set",
+    image_file: "https://m.media-amazon.com/images/I/51ORlClkXYL._AC_.jpg",
+    current_stock: "25",
+  },{
+    name: "Chainmail Necklace",
+    image_file: "https://i.etsystatic.com/34181806/c/3000/2384/0/615/il/e93680/3927535904/il_680x540.3927535904_lx5c.jpg",
+    current_stock: "10",
   },
   {
-    title: "Fall Colors",
-    artist: "DrivingJack",
-    exhibition_date: "October 15, 2018",
-    gallery_id: 3,
-    filename: "06-fall-colors.jpg",
-    description:
-      "Trees with red, orange, yellow leaves reflected on a still lake.",
+    name: "Guitar Pic Earrings",
+    image_file: "https://i.etsystatic.com/24335123/r/il/2e7e0a/3118902789/il_1588xN.3118902789_1wue.jpg",
+    current_stock: "100",
+  },{
+    name: "TShirt: Forgotten",
+    image_file: "https://metaunisex.com/wp-content/uploads/2021/12/Vegvisir2Bviking2Bcompass2Brune2Bprotection2Bsymbol2BCelts2BNordic2BT-Shirt_1_Basic-lBPZ5.jpg",
+    current_stock: "100",
   },
   {
-    title: "Autumn Mountains",
-    artist: "Vitalii_Mamchuk",
-    exhibition_date: "November 3, 2016",
-    gallery_id: 3,
-    filename: "07-autumn-mountains.jpg",
-    description:
-      "Mountains with red and yellow leaves against a background of hazy rolling hills.",
+    name: "TShirt: The Path",
+    image_file: "https://ih1.redbubble.net/image.438277287.8721/ssrco,classic_tee,mens,101010:01c5ca27c6,front_alt,square_product,600x600.u5.jpg",
+    current_stock: "100",
   },
-  {
-    title: "Frozen River",
-    artist: "Vlad Sokolovsky",
-    exhibition_date: "December 24, 2020",
-    gallery_id: 4,
-    filename: "08-frozen-river.jpg",
-    description:
-      "Trees with white frozen branches reflected on a frozen river against a light pink sky.",
-  },
-  {
-    title: "Winter Home",
-    artist: "Smit",
-    exhibition_date: "January 20, 2018",
-    gallery_id: 4,
-    filename: "09-winter-home.jpg",
-    description:
-      "Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.",
-  },
-];
 
-const seedPaintings = () => Painting.bulkCreate(paintingdata);
+const seedInventory = () => Inventory.bulkCreate(inventoryData);
 
-module.exports = seedPaintings;
+module.exports = seedInventory;
