4c366f4bd670e417dcbd27a56bdf65ec84d66dd2
CaseySMiller <CaseySMiller@yahoo.com>
2022-06-18 15:31:39 -0600

    add item form posts to db

--- public/js/update.inv.script.js
+++ public/js/update.inv.script.js
@@ -19,3 +19,40 @@ window.onclick = function (event) {
     modalUpdateInv.style.display = "none";
   }
 };
+
+
+// post form info on submit
+const itemFormEl = document.querySelector('#add-item-form');
+const itemFormHandler = async function(event) {
+    event.preventDefault();
+
+    const nameEl = document.querySelector('#itemName');
+    const stockEl = document.querySelector('#currentStock');
+    const imageEl = document.querySelector('#imageLink');
+    const costEl = document.querySelector('#itemCost');
+    const priceEl = document.querySelector('#salePrice');
+    const orderEl = document.querySelector('#orderLink'); 
+
+    const response = await fetch('/api/inventory/', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: nameEl.value,
+            current_stock: stockEl.value,
+            image_file: imageEl.value,
+            cost: costEl.value,
+            sales_price: priceEl.value,
+            order_link: orderEl.value
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+    document.location.replace('/inventory');
+    } else {
+    alert('Please fill in all required fields');
+    }
+};
+
+if (itemFormEl) {
+    itemFormEl.addEventListener('submit', itemFormHandler);
+};
