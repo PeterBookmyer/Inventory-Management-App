52b37652afa7f8e428ae7cc7194f05c02178339d
CaseySMiller <CaseySMiller@yahoo.com>
2022-06-18 11:53:30 -0600

    login functionality working and recognized by handlebars

---
+++ public/js/logout.js
@@ -0,0 +1,19 @@
+const logOutEl = document.getElementById("logout-button");
+
+const logOut = async function() {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log out');
+    }
+};
+
+logOutEl.addEventListener("click", logOut);
+
+
+
