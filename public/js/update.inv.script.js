6b7681734885667042f73da8eb34a398f819edc0
CaseySMiller <CaseySMiller@yahoo.com>
2022-06-19 11:31:16 -0600

    all update inv buttons now work

--- public/js/update.inv.script.js
+++ public/js/update.inv.script.js
@@ -1,25 +1,27 @@
-var modalUpdateInv = document.getElementsByClassName("modalUpdateInv"); 
+var modalUpdateInv = document.getElementById("modalUpdateInv");
 
-var updateInvBtn = document.getElementsByClassName("updateInvBtn"); // has length === num of rows
+var updateInvBtn = document.getElementsByClassName("updateInvBtn"); // array of all buttons
 
-var modalClose = document.getElementsByClassName("modal-close"); // refernces the submit button
+var modalClose = document.getElementsByClassName("modal-close")[0];
 
+// modal vars
+var options = { startingTop: "0%" };
+var elems = document.querySelectorAll(".modal");
+var instances = M.Modal.init(elems, options);
+var instance = M.Modal.getInstance(modalUpdateInv);
+
+// allow all buttons to open the modal
 document.addEventListener("DOMContentLoaded", function () {
-  var options = { startingTop: "0%" }; //correct
-  var elems = document.querySelectorAll(".modal"); //correct as far as i can tell
-  var instances = M.Modal.init(elems, options); // inits all modals
-  var instance = M.Modal.getInstance(modalUpdateInv[0]); //references the first modal because [0]
-  updateInvBtn[0].onclick = function () { // [0] needs to be a var that determines the row
-    instance.open();
-  };
-  modalClose[0].onclick = function () { // [0] needs to be a var that determines the row
-    instance.close();
-  };
+  for (i=0; i < updateInvBtn.length; i++) {
+    updateInvBtn[i].onclick = function () {
+      instance.open();
+    };
+  }
 });
 
+  
 window.onclick = function (event) {
-  if (event.target == modalUpdateInv[0]) {
-    modalUpdateInv[0].style.display = "none";
+  if (event.target == modalUpdateInv) {
+    modalUpdateInv.style.display = "none";
   }
 };
-
